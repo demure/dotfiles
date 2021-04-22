826d040545e04c4044a60896127d7c171acd0f59
demure <demuredemeanor@gmail.com>
2021-04-22 11:02:12 -0400

    Updated to use a kulgy timg setup for images.

--- config/ranger/scope.sh
+++ config/ranger/scope.sh
@@ -187,7 +187,8 @@ handle_mime() {
         # Image
         image/*)
             # Preview as text conversion
-            img2txt --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4
+            #img2txt --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4
+            timg --color8 -pq -g "${PV_WIDTH}x${PV_HEIGHT}" "${FILE_PATH}" && exit 4
             exiftool "${FILE_PATH}" && exit 5
             exit 1;;
 
