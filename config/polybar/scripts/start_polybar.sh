cf2c7e26c8db225d25470493f592dcf08809abf8
demure <demuredemeanor@gmail.com>
2023-01-28 23:21:52 -0500

    More updates to move to autorandr postswitch, and to fix polybar. Also added multimonitor wallpaper. Oh, and polybar conf name/syntax updates.

--- config/polybar/scripts/start_polybar.sh
+++ config/polybar/scripts/start_polybar.sh
@@ -1,12 +1,13 @@
-#bin/bash
+#!/bin/bash
 ## This is my (demuredemeanor) polybar script for starting polybar and dealing with multiple monitors.
 ## Doesn't address tray, as I don't use it.
 
 ## Kill old polybars
-pkill polybar
+## NOTE: This started needing a -x recently??
+pkill -x polybar
 
-sleep 5
+sleep 3
 
 for m in $(xrandr --query | awk '/ connected/ {print $1}'); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload &
 done
