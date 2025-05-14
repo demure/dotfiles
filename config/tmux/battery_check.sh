e9fe4ac0fa705d4019392bbf81c14607f51b7d04
demure <demuredemeanor@gmail.com>
2025-05-13 23:13:59 -0400

    Updated tmux battery to see GPD Pocket, us current pragmatapro icons, and make while loop dependent on battery script existing.

--- config/tmux/battery_check.sh
+++ config/tmux/battery_check.sh
@@ -37,21 +37,28 @@ fi
 ### End Termux battery ### }}}
 
 
+## GPD Pocket 4
+if [ -e /sys/class/power_supply/BATT/uevent ]; then
+    PERC="$(cat /sys/class/power_supply/BATT/capacity)"
+fi
+
+
 ## Prep Output
+## NOTE: Using pragmata pro font chars
 if [ ! -z "${PERC}" ]; then
     case ${PERC} in
-        100|9[5-9])     ICON="" ;;     ## Full Icon 95-100%
-        8[5-9]|9[0-4])  ICON="" ;;     ## 90% Icon 85-94%
-        7[5-9]|8[0-4])  ICON="" ;;     ## 80% Icon 75-84%
-        6[5-9]|7[0-4])  ICON="" ;;     ## 70% Icon 65-74%
-        5[5-9]|6[0-4])  ICON="" ;;     ## 60% Icon 55-64%
-        4[5-9]|5[0-4])  ICON="" ;;     ## 50% Icon 45-54%
-        3[5-9]|4[0-4])  ICON="" ;;     ## 40% Icon 35-44%
-        2[5-9]|3[0-4])  ICON="" ;;     ## 30% Icon 25-34%
-        1[5-9]|2[0-4])  ICON="" ;;     ## 20% Icon 15-24%
-        [5-9]|1[0-4])   ICON="" ;;     ## 10% Icon 5-14%
-        [0-4])          ICON="" ;;     ## Empty Icon 0-4%
-        *)              ICON="" ;;     ## Catchall Question Icon
+        100|9[5-9])     ICON="󰁹" ;;     ## Full Icon 95-100%
+        8[5-9]|9[0-4])  ICON="󰂂" ;;     ## 90% Icon 85-94%
+        7[5-9]|8[0-4])  ICON="󰂁" ;;     ## 80% Icon 75-84%
+        6[5-9]|7[0-4])  ICON="󰂀" ;;     ## 70% Icon 65-74%
+        5[5-9]|6[0-4])  ICON="󰁿" ;;     ## 60% Icon 55-64%
+        4[5-9]|5[0-4])  ICON="󰁾" ;;     ## 50% Icon 45-54%
+        3[5-9]|4[0-4])  ICON="󰁽" ;;     ## 40% Icon 35-44%
+        2[5-9]|3[0-4])  ICON="󰁼" ;;     ## 30% Icon 25-34%
+        1[5-9]|2[0-4])  ICON="󰁻" ;;     ## 20% Icon 15-24%
+        [5-9]|1[0-4])   ICON="󰁺" ;;     ## 10% Icon 5-14%
+        [0-4])          ICON="󰂎" ;;     ## Empty Icon 0-4%
+        *)              ICON="󰂑" ;;     ## Catchall Question Icon
     esac
 
     #echo -e "${ICON} ${PERC}%\n"
