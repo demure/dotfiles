e8c9fd36aa2daef78e281b729528477ac19a3443
demure <demuredemeanor@gmail.com>
2025-11-05 20:23:47 -0500

    minor inprovements

--- config/waybar/scripts/mediaplayer.py
+++ config/waybar/scripts/mediaplayer.py
@@ -125,9 +125,9 @@ class PlayerManager:
 
         if track_info:
             if player.props.status == "Playing":
-                track_info = " " + track_info
+                track_info = " " + track_info
             else:
-                track_info = " " + track_info
+                track_info = " " + track_info
         # only print output if no other player is playing
         current_playing = self.get_first_playing_player()
         if current_playing is None or current_playing.props.player_name == player.props.player_name:
